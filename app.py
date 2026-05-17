7e94ba9e989f17009602bd50e8fca6dfd1d44bef
Ose-mudiamen <osezuabenjamin50@gmail.com>
2026-05-17 16:47:27 +0100

    Switch to st.secrets for API key

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
 import streamlit as st
 from groq import Groq
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
-
-client = Groq(api_key=os.getenv["GROQ_API_KEY"])
+client = Groq(api_key=st.secrets["GROQ_API_KEY"])
 
 st.set_page_config(page_title="Business FAQ Chatbot", page_icon="")
 st.title("Business FAQ Chatbot")
